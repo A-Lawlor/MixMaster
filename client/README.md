d2b92feb226f773d56e7b5d50a472959e35d8598
A-Lawlor <anthonymichaellawlor@gmail.com>
2023-03-20 23:45:33 -0400

    Initial Commit
    First commit

---
+++ client/README.md
@@ -0,0 +1,5 @@
+# How to run application
+
+Inside the server folder run node server.js to start the connection to the mongoDBAtlas server. Wait for "Successfully connected to MongoDB" message
+
+Inside the client folder run the command npm start. Web application will be running on http://localhost:3000/
---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client"
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+   <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
