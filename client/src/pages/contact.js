e49972ab0f2e972a4031404a2a6a20095cbb9831
Dargueta98 <dargueta1998@gmail.com>
2023-04-08 10:33:24 -0400

    Adding a simple contact form
    Here I am adding a simple contact form that allows users to send a email to mixmasters when they click on "compose"

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { ContactForm } from '../components/contactform'
 import { NavLink } from "react-router-dom";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 function Contact() {
   return (
     <Container fluid className="d-flex flex-column justify-content-center align-items-center">
-      <Row className="justify-content-center align-items-center" style={{ height: "100vh" }}>
-        <Col className="text-center">
-            <h1>Contact Page</h1>
+      <Row className="justify-content-center align-items-center" >
+        <Col xs={12} md={8} className="text-center">
+          <h1>Contact Page</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        <ContactForm />
         </Col>
       </Row>
     </Container>
