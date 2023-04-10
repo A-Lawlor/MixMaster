4f508f9fb06d6bcd980089a17dee38d8c5cbe29d
A-Lawlor <anthonymichaellawlor@gmail.com>
2023-04-10 19:09:34 -0400

    Fancier About Page

--- client/src/pages/mixology.js
+++ client/src/pages/mixology.js
@@ -3,24 +3,24 @@ import { NavLink } from "react-router-dom";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import YoutubeEmbed from "../components/youtube.js";
 import "../css/mixology.css";
-import wikihow from "../images/mixology_pictures/WikiHow.png"
+import wikihow from "../images/mixology_pictures/WikiHow.png";
 
 function Mixology() {
   return (
     <Container>
-      <Row className="justify-content-center align-items-center" style={{ marginTop: "5vh" }}>
-        <Col id="title" className= "text-center" md={10}>
+      <Row className="justify-content-center align-items-center" style={{ marginTop: "3vh" }}>
+        <Col id="mixology_title" className= "text-center" md={10}>
           <p>Mixology Tutorial</p>
         </Col>
       </Row>
-      <Row className="justify-content-center align-items-center" style={{ marginTop: "6vh" }}>
+      <Row className="justify-content-center align-items-center" style={{ marginTop: "2vh" }}>
         <Col id="intro-text" className= "text-center" md={12}>
           <p>Mixing alcoholic drinks is an art that involves crafting cocktails and other beverages from various liquors, mixers, garnishes, and other ingredients. The goal is to balance and enhance the flavors and qualities of each component while also creating a distinctive and delightful drinking experience. Follow along in these tutorials below so you can learn how to properly craft the perfect cocktail everytime!</p>
         </Col>
       </Row>
       <Row className="justify-content-center align-items-center" style={{ marginTop: "6vh"}}>
       <Col className="text-center" md={12}>
-            <NavLink to="https://www.wikihow.com/Make-a-Cocktail" >
+            <NavLink target='_blank' to="https://www.wikihow.com/Make-a-Cocktail" >
               <img id="wikiHow" src={wikihow}/>
             </NavLink>
         </Col>
