edf77b6cc1925ee681cf56afc3e169a9a53baa6c
matthewtran01 <matthewtran2468@yahoo.com>
2023-04-11 21:41:53 -0400

    new headway. need to figure out syntax for objecid

--- routes/drink.js
+++ routes/drink.js
@@ -1,68 +1,48 @@
-const express = require("express");
+  const express = require("express");
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
-const recordRoutes = express.Router();
+  // recordRoutes is an instance of the express router.
+  // We use it to define our routes.
+  // The router will be added as a middleware and will take control of requests starting with path /record.
+  const recordRoutes = express.Router();
 
-// This will help us connect to the database
-const dbo = require("../db/conn");
+  // This will help us connect to the database
+  const dbo = require("../db/conn");
 
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
+  // This help convert the id from string to ObjectId for the _id.
+  const ObjectId = require("mongodb").ObjectId;
 
-// This section will help you get a list of all the records.
-recordRoutes.route("/drink").get(function (req, res) {
- let db_connect = dbo.getDrinksDb("drinks");
- db_connect
-   .collection("drinkfourm")
-   .find({})
-   .toArray()
-   .then ((result) => res.json(result))
-});
- 
-// This section will help you get a single record by id
-recordRoutes.route("/drink/:id").get(function (req, res) {
-  let db_connect = dbo.getDrinksDb();
-  let myquery = { _id: new ObjectId(req.params.id) };
-  console.log("myquery:", myquery);
-  console.log("req.params.id:", req.params.id);
+  // This section will help you get a list of all the records.
+  recordRoutes.route("/drink").get(function (req, res) {
+  let db_connect = dbo.getDrinksDb("drinks");
   db_connect
     .collection("drinkfourm")
-    .findOne(myquery, function (err, result) {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: "An error occurred while retrieving the drink" });
-      } else {
-        res.json(result);
-      }
-    });
-});
- 
-// This section will help you create a new record.
-recordRoutes.route("/drink/add").post(function (req, response) {
- let db_connect = dbo.getDrinksDb();
- let myobj = {
-  image: req.body.image,
-  name: req.body.name,
-  liqour: req.body.liqour,
-  taste: req.body.taste,
-  rating: req.body.rating,
-  likes: req.body.likes,
-  dislikes: req.body.dislikes
- };
- db_connect.collection("drinkfourm").insertOne(myobj, function (err, res) {
-   if (err) throw err;
-   response.json(res);
- });
-});
- 
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
- let db_connect = dbo.getDrinksDb();
- let myquery = { _id: ObjectId(req.params.id) };
- let newvalues = {
-  $set: {
+    .find({})
+    .toArray()
+    .then ((result) => res.json(result))
+  });
+  
+  // This section will help you get a single record by id
+  recordRoutes.route("/drink/:id").get(function (req, res) {
+    let db_connect = dbo.getDrinksDb("drinks");
+    let myquery = { _id: new ObjectId(req.params.id) };
+    console.log("myquery:", myquery);
+    console.log("req.params.id:", req.params.id);
+    db_connect
+      .collection("drinkfourm")
+      .findOne(myquery, function (err, result) {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: "An error occurred while retrieving the drink" });
+        } else {
+          res.json(result);
+        }
+      });
+  });
+
+  // This section will help you create a new record.
+  recordRoutes.route("/drink/add").post(function (req, response) {
+  let db_connect = dbo.getDrinksDb();
+  let myobj = {
     image: req.body.image,
     name: req.body.name,
     liqour: req.body.liqour,
@@ -70,42 +50,26 @@ recordRoutes.route("/update/:id").post(function (req, response) {
     rating: req.body.rating,
     likes: req.body.likes,
     dislikes: req.body.dislikes
-   },
- };
- db_connect
-   .collection("drinkfourm")
-   .updateOne(myquery, newvalues, function (err, res) {
-     if (err) throw err;
-     console.log("1 document updated");
-     response.json(res);
-   });
-});
- 
- 
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
+  };
+  db_connect.collection("drinkfourm").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+  });
+  
+  // This section will help you update a record by id.
+  recordRoutes.route("/update/:id").post(function (req, response) {
   let db_connect = dbo.getDrinksDb();
   let myquery = { _id: ObjectId(req.params.id) };
   let newvalues = {
     $set: {
-     image: req.body.image,
-     name: req.body.name,
-     liqour: req.body.liqour,
-     taste: req.body.taste,
-     rating: req.body.rating,
-     likes: req.body.likes,
-     dislikes: req.body.dislikes
-     
-     
-
-
-     /*<td>{props.record.name}</td>
-    <td>{props.record.liqour}</td>
-    <td>{props.record.taste}</td>
-    <td>{props.record.likes}</td>
-    <td>{props.record.dislikes}</td>
-    <td>{props.record.rating}</td>
-    <td>{props.record.image}</td>*/ 
+      image: req.body.image,
+      name: req.body.name,
+      liqour: req.body.liqour,
+      taste: req.body.taste,
+      rating: req.body.rating,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes
     },
   };
   db_connect
@@ -115,8 +79,10 @@ recordRoutes.route("/update/:id").post(function (req, response) {
       console.log("1 document updated");
       response.json(res);
     });
- });
+  });
   
+
+
  // This section will help you delete a record
  recordRoutes.route("/drink/delete").delete((req, response) => {
    let db_connect = dbo.getDrinksDb();
@@ -128,4 +94,4 @@ recordRoutes.route("/update/:id").post(function (req, response) {
    return response.send(JSON.stringify(myobj));
   });
   
-  module.exports = recordRoutes;
+  module.exports = recordRoutes
