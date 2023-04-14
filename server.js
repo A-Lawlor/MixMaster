9ca00998f25a30d300fcd2119f7bcceb34cf4734
matthewtran01 <matthewtran2468@yahoo.com>
2023-04-14 18:18:03 -0400

    GOT ROUTES WORKING

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
+    
 //const port = process.env.PORT || 5005;
 
 app.listen(process.env.PORT || 5005, function () {
