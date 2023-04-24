b842c652ecb20161cecd41edebee677a6100b12a
Dargueta98 <dargueta1998@gmail.com>
2023-04-23 22:41:34 -0400

    DrinkCard Implemented
    In this commit, I used Material UI in order to create a DrinkCard which will be used throughout the website.
    
    -Current you can select how many stars (for rating) and you can "like" as well, but it IS NOT currently connected to the backend.

--- client/src/pages/Drinkpage.jsx
+++ client/src/pages/Drinkpage.jsx
@@ -1,7 +1,61 @@
+
+/*import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';*/
+
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { styled, createTheme } from '@mui/material/styles';
+import { Box } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import RecommendIcon from '@mui/icons-material/Recommend';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Rating from '@mui/material/Rating';
+import LocalBarIcon from '@mui/icons-material/LocalBar';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 
 export default function Drinkpage() {
+
+const theme = createTheme({
+  palette: {
+    // your other color settings
+    customGradient: 'linear-gradient(135deg, #6aa84f 0%, #e8d5fb 100%)',
+  },
+  // your other theme settings
+});
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const [value, setValue] = useState(0);
+
+  const handleRatingChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   console.log("here");
   const [drink, setDrink] = useState(null);
   const { id } = useParams();
@@ -23,7 +77,62 @@ export default function Drinkpage() {
       {ingredient.name}: {ingredient.amount}
     </p>
   ));
+
   return (
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+    <Card sx={{ maxWidth: 345, marginTop: '30px', borderRadius: 8,background: theme.palette.customGradient }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        title="Mismosa"
+      />
+      <CardMedia
+        component="img"
+        image={drink.picture_url}
+        sx={{ width: '100%' }}
+        alt="drink image"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        {drink.name}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <RecommendIcon />
+        </IconButton>
+        <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={handleRatingChange}/>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <LocalBarIcon />
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        <Typography paragraph><em><u>Drink Ingredients:</u></em></Typography>
+          <Typography paragraph sx={{ fontStyle: 'italic' }}>
+          {ingredients}
+            </Typography>
+            <Typography>
+          About:{drink.about}
+            </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+    </Box>
+    
+    /*
     <div style={{ textAlign: 'center' , marginTop: '150px'}}>
       <h1>{drink.name}</h1>
       <p>Description: {drink.about}</p>
@@ -40,29 +149,6 @@ export default function Drinkpage() {
       </div>
       
     </div>
+    */
   );
 }
-
-/*import React, { useState, useEffect } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
-import axios from 'axios';
-
-const Drinkpage = _ =>{
-
-
-  const {state} = useLocation();
-
-
-  return (
-    <div>
-      <h1>{state.name}</h1>
-      <p>Liquor: {state.liqour}</p>
-      <p>Taste: {state.taste}</p>
-      <p>Rating: {state.rating}</p>
-      <p>Likes: {state.likes}</p>
-      <p>Dislikes: {state.dislikes}</p>
-    </div>
-  );
-  };
-
-  export default Drinkpage*/ 
